980c7ff7f8b097fb94cc07a6d4ed6ea447461334
htdat2000 <12a6.ht.dat@gmail.com>
2023-10-08 09:03:34 +0700

    Player Moving

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] protected float moveSpeed = 5;
+
+        protected Rigidbody2D rb;
+
+        protected void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        protected void FixedUpdate()
+        {
+            Move();            
+        }
+
+        protected void Move()
+        {
+            Debug.Log("Move");
+            Vector2 moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Debug.Log(moveDir);
+            rb.velocity = moveDir.normalized * moveSpeed;        
+        }
+    }
+}
